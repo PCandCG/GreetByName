efc73974a1771e34569828614cacbd8ed6a54d46
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-05-27 13:50:56 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string username;
+	cout << "Please enter your name: ";
+	cin >> username;
+	cout << "Hello " << username;
+	return 0;
+}
